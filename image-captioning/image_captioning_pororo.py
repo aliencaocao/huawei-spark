419434e0df6e2e5ec44b97f82dd56655bf7bbdde
Billy Cao <aliencaocao@gmail.com>
2022-03-05 12:15:46 +0800

    Fix local loading of weights for Pororo

--- image-captioning/image_captioning_pororo.py
+++ image-captioning/image_captioning_pororo.py
@@ -1,5 +1,8 @@
+import os
 import sys
+os.environ['TORCH_HOME'] = 'model/'
 sys.path.append('pororo/')
+
 from pororo import Pororo
 caption = Pororo(task='caption', lang='en')  # will download model first time
 print(caption('office.jpg'))
