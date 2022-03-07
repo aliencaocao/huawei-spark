e838adbb0af67c3dfbe08bfcb38ecea8eb0b39ac
Billy Cao <aliencaocao@gmail.com>
2022-03-07 22:41:58 +0800

    Fix OCR code

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -13,7 +13,7 @@ headers = {'Content-Type': 'application/json', 'X-Auth-Token': token}
 
 image_url = 'https://www.minitool.com/images/uploads/news/2019/07/how-to-check-pc-specs-windows-10/how-to-check-pc-specs-windows-10-2.png'  # can be Huawei OBS URL or any other
 # image = ''  # base64 encoded image, set to 'image' attribute of payload instead of 'url'
-payload = {'url': image_url, 'detect_direction': 'true'}  # 'terrorism' for unknown reason says IAM user not authorized but porn works
+payload = {'url': image_url}  # 'detect_direction': 'true' for some reason don't work here although its documented in docs
 
 print('POSTing request to the server')
 response = requests.post(url, headers=headers, json=payload, verify=False)
