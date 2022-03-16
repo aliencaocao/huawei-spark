447dd3c37229c277e0ac948c87f88d6f29480f6c
Ho Wing Yip <sploooosh884@gmail.com>
2022-03-16 21:33:41 +0800

    Display user's own listings in profile

--- ecoshop-frontend/src/Components/Profile.js
+++ ecoshop-frontend/src/Components/Profile.js
@@ -1,11 +1,13 @@
 import "../css/profile.css";
 import "../css/global.css"
 import EnergySavingsLeafIcon from '@mui/icons-material/EnergySavingsLeaf';
-import { Avatar, Box, Card, CardContent, Divider } from "@mui/material";
+import { Avatar, Box, Card, CardContent, Divider, Grid, Paper } from "@mui/material";
 import { useState, useEffect, Fragment } from "react";
+import { AccountCircle, Person } from "@mui/icons-material";
 
 const Profile = (props) => {
   const [userInfo, setUserInfo] = useState(null);
+  const [userListings, setUserListings] = useState([]);
 
   let tokenData;
   if (window.token) {
@@ -23,15 +25,30 @@ const Profile = (props) => {
         });
   };
 
-  useEffect(loadProfileDetails, []);
+  const loadUserListings = () => {
+    fetch(`${window.globalURL}/product/owned`, {
+      method: "POST",
+      headers: { "Authorization": window.token },
+    })
+        .then((res) => res.json())
+        .then(({ listings }) => {
+          setUserListings(listings);
+        });
+  };
+
+  useEffect(() => {
+    loadProfileDetails();
+    loadUserListings();
+  }, []);
 
   return (
     <main>
-      <h1>Your Profile</h1>
+      <h1>My Profile</h1>
       {userInfo !== null && (
         <Fragment>
           <Box id="profile-top">
-            <Avatar src="https://ecoshop-content.obs.ap-southeast-3.myhuaweicloud.com/user-image/dievfAvZhNmFJGAtQrVf1h" />
+            {/* <Avatar src="https://ecoshop-content.obs.ap-southeast-3.myhuaweicloud.com/user-image/dievfAvZhNmFJGAtQrVf1h" /> */}
+            <Avatar><AccountCircle /></Avatar>
             <Box id="profile-top-text">
               <h1>{tokenData.username}</h1>
               <h2>Joined {
@@ -52,11 +69,29 @@ const Profile = (props) => {
                 <span>Green Points</span>
               </Box>
               <Divider orientation="vertical" flexItem />
-              <Box>
-                Buy more things to get more Green Points!
-              </Box>
+              <Box>Buy more things to get more Green Points!</Box>
             </CardContent>
           </Card>
+
+          <h2>My Listings</h2>
+          {
+            userListings.length > 0 ? (
+              <Grid container spacing={2} className="profile-listings">
+                {userListings.map((listing) => (
+                  <Grid item xs={6} key={listing.id}>
+                    <Paper>
+                      <img src={window.mediaURL + listing.obs_image} />
+                      <Box className="profile-listing-text">
+                        <h3>{listing.name}</h3>
+                        <div>${listing.price}</div>
+                        <div>Quantity: {listing.quantity}</div>
+                      </Box>
+                    </Paper>
+                  </Grid>
+                ))}
+              </Grid>
+            ) : "No listings."
+          }
         </Fragment>
       )}
     </main>
